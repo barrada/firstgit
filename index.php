9441a87fecd79b96be66a647d1559f2c3eb96bd2
barrada <barrada@me.com>
2016-09-17 01:32:58 +0300

    added about us in index file

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 	<div class="intro">
 		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur soluta atque fugit tenetur. Reiciendis, hic nemo assumenda accusamus, maxime enim ipsa autem distinctio minus iusto in. Sed a, iste voluptatibus?
 	</div>
+
+	<div class="about-us">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos sed quod quaerat commodi ipsa sint sit dolor velit minima dolore, facere at sunt animi odio, cum iusto quia et, molestiae.
+	</div>
 </body>
 </html>
