ae238c91456906619ab8923d5f450676feddd8fe
barrada <barrada@me.com>
2016-09-17 01:27:13 +0300

    added intro to index file

--- index.php
+++ index.php
@@ -6,5 +6,8 @@
 </head>
 <body>
 	<h1>Testing Git</h1>
+	<div class="intro">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur soluta atque fugit tenetur. Reiciendis, hic nemo assumenda accusamus, maxime enim ipsa autem distinctio minus iusto in. Sed a, iste voluptatibus?
+	</div>
 </body>
 </html>
